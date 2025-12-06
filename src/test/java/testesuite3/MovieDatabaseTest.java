74cb159f120c49a9881554394a6819c4a3f0544c
LEI-106090 <tmcas2@iscte-iul.pt>
2025-12-06 14:05:32 +0000

    Add movie database test -> Find movie information

---
+++ src/test/java/testesuite3/MovieDatabaseTest.java
@@ -0,0 +1,41 @@
+package testesuite3;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieDatabaseTest
+{
+    private MovieDatabasePage mainPage;
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+        mainPage = new MovieDatabasePage();
+        mainPage.openPage("https://vaadin-database-example.demo.vaadin.com/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
+    }
+
+    @Test
+    void checkIMDB() throws InterruptedException
+    {
+        String movieName = "Law Abiding Citizen";
+        MovieDatabasePage.MovieRow row = mainPage.findInfoAboutMovie( movieName);
+
+        assertEquals(movieName, row.title().getText(), "Nao encontrou o filme correto");
+        assertEquals("2009", row.year().getText(), "O ano do filme não está correto.");
+        assertEquals("F. Gardy Gray", row.director().getText(), "O director encontrado não está correto.");
+        assertEquals("https://www.imdb.com/title/tt1197624/", row.link(), "Link errado.");
+    }
+
+}
